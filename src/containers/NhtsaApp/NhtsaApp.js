270c45f9295a2cf0caf15b87f6e5c242d373b8f3
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-02-15 23:46:04 -0500

    Refactor NhtsaApp container component into a functional component

--- src/containers/NhtsaApp/NhtsaApp.js
+++ src/containers/NhtsaApp/NhtsaApp.js
@@ -1,83 +1,15 @@
 import styles from './NhtsaApp.css';
 
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Overlay from '../../components/Overlay/Overlay';
 import Dashboard from '../../components/Dashboard/Dashboard';
-import ButtonContainer from '../../components/ButtonContainer/ButtonContainer';
+import { Hood } from '../../containers/Hood/Hood';
 import { verticalScrollThreshold } from '../../config/app-constants';
-
-export default class NhtsaApp extends Component {
-    render() {
-        const { browser } = this.props;
-        const { dashboardDimensions, makeWrapperDimensions, overlaySplatDimensions } = this.props.layout;
-        const activeTab = this.props.activeTab;
-        const hoodAction = this.props.hoodAction;
-        const aboutVisibility = this.props.aboutVisibility;
-        const chartData = this.props.chartData;
-        let newBackgroundX = (dashboardDimensions.offsetLeft + dashboardDimensions.width) - 465;
-        let newBackgroundY;
-        let newOverflowY = 'hidden';
-        if (window.innerHeight < verticalScrollThreshold) {
-            newBackgroundY = verticalScrollThreshold - 323;
-        } else {
-            newBackgroundY = window.innerHeight - 323;
-        }
-        if (browser.height < verticalScrollThreshold) {
-            newOverflowY = 'visible';
-        }
-        const nhtsaAppStyle = {
-            backgroundPositionX: newBackgroundX,
-            backgroundPositionY: newBackgroundY,
-            overflowY: newOverflowY
-        };
-        return (
-            <div
-                className={
-                    styles.root
-                }
-                style={
-                    nhtsaAppStyle
-                }
-            >
-                <Overlay
-                    makeWrapperDimensions={
-                        makeWrapperDimensions
-                    }
-                    overlaySplatDimensions={
-                        overlaySplatDimensions
-                    }
-                />
-                <Dashboard
-                    tab={
-                        activeTab
-                    }
-                    aboutVisibility={
-                        aboutVisibility
-                    }
-                    chartData={
-                        chartData
-                    }
-                />
-                <footer>
-                    © 2014 <a href="http://www.zoomdata.com/">Zoomdata</a>, Inc. <a href="http://www.zoomdata.com/contact">Contact</a> <a href="http://www.zoomdata.com/terms">Legal</a>
-                </footer>
-                <ButtonContainer
-                    dashboardDimensions={
-                        dashboardDimensions
-                    }
-                    hoodAction={
-                        hoodAction
-                    }
-                />
-            </div>
-        )
-    }
-}
 
 // Which props do we want to inject, given the global state?
 // Note: use https://github.com/faassen/reselect for better performance.
-function mapStateToProps(state) {
+const mapStateToProps = (state) => {
     return {
         browser: state.browser,
         layout: state.layout,
@@ -86,6 +18,61 @@ function mapStateToProps(state) {
         aboutVisibility: state.aboutVisibility,
         chartData: state.chartData
     }
+}
+
+export default function NhtsaApp (state) {
+    const { browser, layout, activeTab, hoodAction, aboutVisibility, chartData } = state;
+    const { dashboardDimensions, makeWrapperDimensions, overlaySplatDimensions } = layout;
+    let newBackgroundX = (dashboardDimensions.offsetLeft + dashboardDimensions.width) - 465;
+    let newBackgroundY;
+    let newOverflowY = 'hidden';
+    if (window.innerHeight < verticalScrollThreshold) {
+        newBackgroundY = verticalScrollThreshold - 323;
+    } else {
+        newBackgroundY = window.innerHeight - 323;
+    }
+    if (browser.height < verticalScrollThreshold) {
+        newOverflowY = 'visible';
+    }
+    const nhtsaAppStyle = {
+        backgroundPositionX: newBackgroundX,
+        backgroundPositionY: newBackgroundY,
+        overflowY: newOverflowY
+    };
+    return (
+        <div
+            className={
+                styles.root
+            }
+            style={
+                nhtsaAppStyle
+            }
+        >
+            <Overlay
+                makeWrapperDimensions={
+                    makeWrapperDimensions
+                }
+                overlaySplatDimensions={
+                    overlaySplatDimensions
+                }
+            />
+            <Dashboard
+                tab={
+                    activeTab
+                }
+                aboutVisibility={
+                    aboutVisibility
+                }
+                chartData={
+                    chartData
+                }
+            />
+            <footer>
+                © 2014 <a href="http://www.zoomdata.com/">Zoomdata</a>, Inc. <a href="http://www.zoomdata.com/contact">Contact</a> <a href="http://www.zoomdata.com/terms">Legal</a>
+            </footer>
+            <Hood />
+        </div>
+    )
 }
 
 // Wrap the component to inject dispatch and state into it
