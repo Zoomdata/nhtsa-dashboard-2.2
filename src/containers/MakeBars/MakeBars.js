08bf3fd98c99d7d8a72ec75306cf74c6cbff4468
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-02-16 13:57:37 -0500

    Small syntax change to mapStateToProps function declaration in MakeBars container

--- src/containers/MakeBars/MakeBars.js
+++ src/containers/MakeBars/MakeBars.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
 import MakeBarChart from '../../components/MakeBarChart/MakeBarChart';
 
-const mapStateToProps = ((state) => {
+const mapStateToProps = (state) => {
     return {
         data: state.chartData.makeData.data
     }
-})
+}
 
 export const MakeBars = connect(mapStateToProps)(MakeBarChart);
