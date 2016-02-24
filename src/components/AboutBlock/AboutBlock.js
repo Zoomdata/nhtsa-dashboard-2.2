1d40f3750eef5b1436b89c2e8692d5054d10d0d7
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-02-24 00:18:55 -0500

    Add animation for opacity to AboutBlock when clicking 'About Demo' and 'Return'

--- src/components/AboutBlock/AboutBlock.js
+++ src/components/AboutBlock/AboutBlock.js
@@ -5,24 +5,41 @@ import AboutHeader from '../AboutHeader/AboutHeader';
 import AboutDescription from '../AboutDescription/AboutDescription';
 import ZDWebsiteButton from '../ZDWebsiteButton/ZDWebsiteButton';
 import CloseAboutButton from '../CloseAboutButton/CloseAboutButton';
+import { VelocityComponent } from 'velocity-react';
 
 const AboutBlock = ({aboutVisibility}) => {
+    let animationProps;
+    if (aboutVisibility === 'CLOSE_ABOUT') {
+        animationProps = {
+            duration: 1000,
+            animation: {
+                opacity: 0
+            },
+            display: 'auto'
+        }
+    } else {
+        animationProps = {
+            duration: 1000,
+            animation: {
+                opacity: 1
+            },
+            display: 'block'
+        }
+    }
     return (
-        <div
-            className={
-                aboutVisibility === 'CLOSE_ABOUT' ?
-                styles.normal :
-                styles.active
-            }>
-            <AboutHeader />
-            <AboutDescription />
-            <a
-                target="_blank"
-                href="http://zoomdata.com">
-                <ZDWebsiteButton />
-            </a>
-            <CloseAboutButton />
-        </div>
+        <VelocityComponent {...animationProps}>
+            <div
+                className={styles.root}>
+                <AboutHeader />
+                <AboutDescription />
+                <a
+                    target="_blank"
+                    href="http://zoomdata.com">
+                    <ZDWebsiteButton />
+                </a>
+                <CloseAboutButton />
+            </div>
+        </VelocityComponent>
     )
 };
 
