24ac374a0853dab94f144e5b99f98db9188bf9b2
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-03-14 09:46:48 -0400

    Add time field to source queries

--- src/config/queries/gridData.js
+++ src/config/queries/gridData.js
@@ -2,6 +2,6 @@ export const source = 'Vehicle Complaints';
 export const queryConfig = {
     limit: 50,
     offset: 0,
-    restrictions: [],
+    restrictions: [{"path":"failtimestamp_real","operation":"BETWEEN","value":["1995-01-01 08:00:00.000","2014-07-10 07:00:00.000"]}],
     streamSourceId: ''
 };
