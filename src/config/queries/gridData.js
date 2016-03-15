f74d6cc2f1030fae83bfb8bcac317a9ccb68fb31
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-03-15 12:23:52 -0400

    Include sortCfgs parameter in grid query to prevent duplicate rows from showing across pages

--- src/config/queries/gridData.js
+++ src/config/queries/gridData.js
@@ -3,5 +3,11 @@ export const queryConfig = {
     limit: 50,
     offset: 0,
     restrictions: [{"path":"failtimestamp_real","operation":"BETWEEN","value":["1995-01-01 08:00:00.000","2014-07-10 07:00:00.000"]}],
-    streamSourceId: ''
+    streamSourceId: '',
+    sortCfgs: [
+        {
+            dir: 'asc',
+            name: 'failtimestamp_real'
+        }
+    ]
 };
