30cd6a6008396354267fa1e80be602470f82322a
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-03-15 14:44:39 -0400

    Fix production oauth client_id

--- src/config/zoomdata-connections/production.js
+++ src/config/zoomdata-connections/production.js
@@ -9,7 +9,7 @@ export const server = {
         path: '/zoomdata'
     },
     oauthOptions: {
-        client_id: "bmh0c2FfY2xpZW50MTQ1ODA2NDI1MjY5MmI2MjFkNDI5LTUwNDgtNDIxYS1iZTZiLTc1MDZmYzFhMzA1Mg==",
+        client_id: "bmh0c2FfY2xpZW50MTQ1ODA2NzM4MTE3NDdkNzAxZGIzLTA3MDMtNDk4Mi1iNThiLTQ4NzU2OTZkOTYwNw==",
         redirect_uri: "http://demos.zoomdata.com/nhtsa-dashboard-2.2/index.html",
         auth_uri: "https://preview.zoomdata.com/zoomdata/oauth/authorize",
         scope: ['read']
