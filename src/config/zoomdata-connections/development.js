77656b31416213c38500ffb03cea9cea46331358
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-04-25 21:44:30 -0400

    Update dev connection

--- src/config/zoomdata-connections/development.js
+++ src/config/zoomdata-connections/development.js
@@ -3,15 +3,15 @@ export const server = {
         access_token: ''
     },
     application: {
-        secure: false,
+        secure: true,
         host: 'localhost',
-        port: 8080,
+        port: 8443,
         path: '/zoomdata'
     },
     oauthOptions: {
         client_id: "bmh0c2FfY2xpZW50MTQ1OTM0Nzc1NTg0MjQ4YzNiNzdmLTI1NTYtNGMyMS1iODYyLTg2NDRiYjA5ZDlmZA==",
         redirect_uri: "http://localhost:3000/index.html",
-        auth_uri: "http://localhost:8080/zoomdata/oauth/authorize",
+        auth_uri: "https://localhost:8443/zoomdata/oauth/authorize",
         scope: ['read']
     }
 };
