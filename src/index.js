6d9839e3c2a7462bf6adaf053a872beb6cdb7e55
Jonathan Avila <jonathanavila@Jonathans-MacBook-Pro.local>
2016-05-16 20:32:36 +0300

    Change server to Production (fix 2)

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import NhtsaApp from './components/NhtsaApp/NhtsaApp';
 import Provider from './stores/Provider';
 import store from './stores/UiState';
 import { oauthInit } from './config/oauth';
-import { server } from './config/zoomdata-connections/development';
+import { server } from './config/zoomdata-connections/production';
 
 const { oauthOptions } = server;
 oauthInit(oauthOptions); // Authenticate against Zoomdata
